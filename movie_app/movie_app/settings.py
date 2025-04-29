31405af730ab6bc7fb1b150ce71d37261549f8f3
Dyvid12 <vitra1812@gmail.com>
2025-04-29 16:06:34 +0700

    add new

--- movie_app/movie_app/settings.py
+++ movie_app/movie_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(@t9r@#!nv3p@l+tu$b)zr(g(gddorx#x*ixh=nn!!8p01)+s0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
